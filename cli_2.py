c1be03bd1339465c243f0ddc1e6502d2b20da953
Flavio de Souza <flaviodesouzza@gmail.com>
2026-01-28 14:17:47 -0300

    Docstring cli_2.py
    Definição da docstring do projeto

---
+++ cli_2.py
@@ -0,0 +1,6 @@
+"""
+O projeto implementa uma aplicação de linha de comando capaz de registrar
+dados fornecidos pelo usuário e persistí-los em um arquivo CSV, garantindo
+organização, validação de entrada e histórico imutável.
+"""
+
